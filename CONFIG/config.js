963c6cd9f5cc5284ea38918ce1907d7328bef0b8
CPDV-REGISTER <gariasp@cpdv.cl>
2025-08-08 12:33:19 -0400

    Add files via upload

---
+++ CONFIG/config.js
@@ -0,0 +1,9 @@
+// Archivo de configuración global del sistema
+// Aquí guardaremos las URLs del backend (Google Apps Script) y otros parámetros.
+
+// URL del script de Google Apps Script para manejar datos
+const SCRIPT_URL_GENERAR_QR = "https://script.google.com/macros/s/AKfycbz0fGEWqaXxtdLKQDsWr5k5gbu1oVGKnlQtl6qFj3ijenkzISP-HrrMIlqtMcO-GiBbFA/exec";
+const SCRIPT_URL_VERIFICAR_QR = "https://script.google.com/macros/s/AKfycbz0fGEWqaXxtdLKQDsWr5k5gbu1oVGKnlQtl6qFj3ijenkzISP-HrrMIlqtMcO-GiBbFA/exec";
+
+// Tiempo de validez del QR en horas (por defecto 4)
+const QR_VALIDEZ_HORAS = 4;
